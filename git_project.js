2f9ebbcfb669432d045998e76420e436a067f8e1
Blake Watton-Dirkse <blakewd16@gmail.com>
2024-08-07 11:25:00 +0200

    Added the 3 Data groups and stored them within a variable

--- git_project.js
+++ git_project.js
@@ -1,1 +1,24 @@
-console.log('This is a random message generator');
+console.log('This is a random Inspirational Messenger');
+
+// stores data of message group 1
+const data1 = [ "It takes courage to grow up and become who you really are. — E.E. Cummings",
+    "Your self-worth is determined by you. You don't have to depend on someone telling you who you are. — Beyoncé",
+    "Nothing is impossible. The word itself says 'I'm possible!' — Audrey Hepburn",
+    "Keep your face always toward the sunshine, and shadows will fall behind you. — Walt Whitman",
+    "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You're on your own. And you know what you know. And you are the guy who'll decide where to go. — Dr. Seus" ];
+// stores data of message group 2
+const data2 = [ "Attitude is a little thing that makes a big difference. — Winston Churchill",
+    "To bring about change, you must not be afraid to take the first step. We will fail when we fail to try. — Rosa Parks",
+    "All our dreams can come true, if we have the courage to pursue them. — Walt Disney",
+    "Don't sit down and wait for the opportunities to come. Get up and make them. — Madam C.J. Walker",
+    "Champions keep playing until they get it right. — Billie Jean King" ];
+// stores data of message group 3
+const data3 = [ "I am lucky that whatever fear I have inside me, my desire to win is always stronger. — Serena Williams",
+    "You are never too old to set another goal or to dream a new dream. — C.S. Lewis",
+    "It is during our darkest moments that we must focus to see the light. — Aristotle",
+    "Believe you can and you're halfway there. — Theodore Roosevelt",
+    "Life shrinks or expands in proportion to one’s courage. — Anaïs Nin" ];
+
+// stores all 3 arrays
+const messages = [data1, data2, data3];
+
